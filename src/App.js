058ba39910ec4a677dc467ac99758e4e4621fd85
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-01 20:45:40 +0530

    with get method and db json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
+import axios from "axios";
+import { useState } from "react";
+
+const getData = () => {
+  const config = {
+    url: "db.json",
+    method: "get"
+  };
+  return axios(config);
+};
+
+getData().then((res) => {
+  console.log(res);
+});
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  return <div className="App"></div>;
 }
